495e2d50e16885619485f42f44cc9dde02a6cc75
valentin-helies <89925635+valentin-helies@users.noreply.github.com>
2022-02-22 11:59:26 +0100

    Update DashboardController.php

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 
