db8b64a943816d1d0f7c9d4b6a1bc0859d52601b
Valentin HELIES <valentin@2le.net>
2024-01-19 15:59:16 +0100

    Fix deprecated 2le/crudit#365

--- src/LleDashboardBundle.php
+++ src/LleDashboardBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class LleDashboardBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
     }
