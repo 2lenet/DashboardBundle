5e91918f3865ce7f3fbd95e28946b898e8eff45e
Quentin <quentin@2le.net>
2024-04-19 17:02:36 +0200

    Add SF7 compatibility

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Contracts\Cache\CacheInterface;
