1f206efac08682748d2476c9c0ba257a0ef7658f
Bastien <bastien@2le.net>
2024-10-25 09:42:51 +0200

    Reload only on width modification

--- src/Resources/public/dashboard/print.js
+++ src/Resources/public/dashboard/print.js
@@ -1,10 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!****************************!*\
-  !*** ./assets/js/print.js ***!
-  \****************************/
-window.addEventListener('load', function () {
-  print();
-});
-/******/ })()
-;
+window.addEventListener("load",(function(){print()}));
